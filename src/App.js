298a8df66918ede770d50a3dc88dbed40bd9070e
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-05-10 18:33:48 -0500

    adding cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Card from "./components/Card";
+import ProjectCard from "./components/ProjectCard";
 import Footer from "./components/Footer";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/form/contactForm";
 import TextInfo from "./components/TextInfo";
+import Project1 from "./images/Project1.png";
+import Project2 from "./images/Project2.png";
+import Project3 from "./images/Project3.png";
 
 function App() {
     return (
@@ -14,7 +17,27 @@ function App() {
             <Navbar />
             <TextInfo />
             <AboutMe />
-            <Card />
+            <ProjectCard 
+                image={Project1} 
+                projectTitle="Navigation App" 
+                projectDescription="This project was the first javascript project I've ever done. " 
+                shareUrl="https://github.com/ADBLOW-TEAM-1/Project-ADBLOW" 
+                learnUrl="https://github.com/TonyWorldChanger"
+            />
+             <ProjectCard 
+                image={Project2} 
+                projectTitle="Navigation App" 
+                projectDescription="This project was the first javascript project I've ever done. " 
+                shareUrl="https://github.com/NTumminaro/Sum-Of-Best" 
+                learnUrl="https://github.com/TonyWorldChanger"
+            />
+             <ProjectCard 
+                image={Project3} 
+                projectTitle="Navigation App" 
+                projectDescription="This project was the first javascript project I've ever done. " 
+                shareUrl="https://github.com/TonyWorldChanger/Challenge13-E-Commerce_BackEnd" 
+                learnUrl="https://github.com/TonyWorldChanger"
+            />
             <Contact />
             <Footer />
           
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,29 +5,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Project1 from "../styles/Project1.PNG";
 
 
-export default function ImagMediaCard() {
+
+export default function ProjectCard({image, projectTitle, projectDescription, shareUrl, learnUrl}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="180"
-        image={Project1}
+        image={image}
         alt="Navigation App"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Navigation App
+          Navigation App {projectTitle}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          This project was the first javascript project I've ever done. 
+          This project was the first javascript project I've ever done. {projectDescription}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" href="https://github.com/ADBLOW-TEAM-1/Project-ADBLOW">Share </Button>
-        <Button size="small" href= "https://github.com/TonyWorldChanger">Learn More</Button>
+        <Button size="small" href={shareUrl}>Share </Button>
+        <Button size="small" href={learnUrl}>Learn More</Button>
       </CardActions>
     </Card>
   );
